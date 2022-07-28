7a6021e5588a65afd205d9c7c4bead05aafe80ce
DeniedAccessLife <89941208+DeniedAccessLife@users.noreply.github.com>
2022-07-28 13:29:51 +0300

    Add files via upload

---
+++ Blocker/Blocker/Host.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.IO;
+using Microsoft.Win32;
+
+namespace Blocker
+{
+    class Host
+    {
+        public static string file;
+
+        public static void Initialization()
+        {
+            RegistryKey key = Registry.LocalMachine.OpenSubKey(@"SYSTEM\CurrentControlSet\Services\Tcpip\Parameters\");
+            string value = key.GetValue("DataBasePath").ToString();
+            key.Close();
+
+            file = Environment.ExpandEnvironmentVariables(value + @"\hosts").ToLower();
+
+            File.Copy(file, file + ".back", true);
+            File.SetAttributes(file, File.GetAttributes(file) & ~FileAttributes.ReadOnly);
+        }
+
+        public static void Add(string data)
+        {
+            string text = string.Empty;
+            using (StreamReader reader = new StreamReader(file))
+            {
+                using (StreamWriter writer = new StreamWriter(file, true))
+                {
+                    text = reader.ReadToEnd();
+
+                    if (!text.Contains(data))
+                    {
+                        writer.WriteLine("127.0.0.1 " + data);
+                    }
+
+                    writer.Close();
+                    reader.Close();
+                }
+            }
+        }
+
+        public static void Rem(string data)
+        {
+            string line = string.Empty;
+            using (StreamReader reader = new StreamReader(file))
+            {
+                using (StreamWriter writer = new StreamWriter(file + ".temp"))
+                {
+                    while ((line = reader.ReadLine()) != null)
+                    {
+                        if (!string.IsNullOrWhiteSpace(line) && line != "127.0.0.1 " + data)
+                        {
+                            writer.WriteLine(line);
+                        }
+                    }
+
+                    writer.Close();
+                    reader.Close();
+
+                    File.Delete(file);
+                    File.Move(file + ".temp", file);
+                }
+            }
+        }
+    }
+}
---
+++ Blocker/Blocker/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+
+namespace Blocker
+{
+    class Program
+    {
+        static void Start(string[] args)
+        {
+            string command = args[0].Remove(0, 1).ToUpper();
+
+            switch (command)
+            {
+                case "HELP":
+                    {
+                        Console.WriteLine("Usage: <command> <params> - execute command");
+                        Console.WriteLine();
+                        Console.WriteLine("Command list: ");
+                        Console.WriteLine("/help - show information");
+                        Console.WriteLine("/add <url> - add new host");
+                        Console.WriteLine("/rem <url> - remove host");
+                        break;
+                    }
+                case "ADD":
+                    {
+                        try
+                        {
+                            Host.Add(args[1]);
+                        }
+                        catch (IndexOutOfRangeException)
+                        {
+                            Console.WriteLine("The <url> argument is required!");
+                        }
+                        break;
+                    }
+                case "REM":
+                    {
+                        try
+                        {
+                            Host.Rem(args[1]);
+                        }
+                        catch (IndexOutOfRangeException)
+                        {
+                            Console.WriteLine("The <url> argument is required!");
+                        }
+                        break;
+                    }
+                default:
+                    {
+                        Console.WriteLine("Unknown command: " + args[0].ToLower());
+                        break;
+                    }
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Utils.Install();
+            Host.Initialization();
+
+            try
+            {
+                Utils.Copyright(Host.file);
+
+                while (true)
+                {
+                    Console.Write("blocker> ");
+                    string command = Console.ReadLine();
+
+                    if (!string.IsNullOrEmpty(command))
+                    {
+                        Start(command.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries));
+                        Console.WriteLine();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Utils.Exception(ex.Message);
+            }
+
+            Console.ReadKey();
+            Environment.Exit(0);
+        }
+    }
+}
